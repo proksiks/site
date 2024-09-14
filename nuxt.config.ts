6b23fedd446c8dcba2cd428f5f786c4cc676e765
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-09-14 22:50:44 +0400

    Попытка поправить импорты с учётом ghpages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -71,6 +71,7 @@ export default defineNuxtConfig({
       ],
     },
     workbox: {
+      globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       cleanupOutdatedCaches: true,
       //modifyURLPrefix: {
       //  "/site/": "/",
