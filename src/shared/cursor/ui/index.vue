37da1a0f6c55bfaf77b44d0b1238de7ee30c0817
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-09-18 23:48:55 +0400

    Поправил анимацию курсора, сделал через watch

--- src/shared/cursor/ui/index.vue
+++ src/shared/cursor/ui/index.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="custom-cursor" ref="cursor" :style="{ left: getXCoordinates, top: getYCoordinates }"></div>
+  <div class="custom-cursor" ref="cursor"></div>
 </template>
 
 <script lang="ts" setup>
-  import { usePointer } from "@vueuse/core";
+  import { usePointer, useWindowSize } from "@vueuse/core";
   import { reactive } from "vue";
+
+  const { $gsap: gsap } = useNuxtApp();
 
   const pointer = reactive(usePointer());
   const cursor = ref();
 
-  const getXCoordinates = computed(() => {
-    return pointer.x - 100 + "px";
+  const { width } = useWindowSize();
+  const centerX = width.value / 2;
+  const scale = computed(() => {
+    if (centerX / pointer.x < 1) {
+      return centerX / pointer.x;
+    }
+    if (width.value / pointer.x > 1) {
+      const x = Math.abs(centerX - pointer.x) + centerX;
+      return centerX / x;
+    }
   });
-  const getYCoordinates = computed(() => {
-    return pointer.y - 100 + "px";
+
+  watch(pointer, () => {
+    gsap.set(cursor.value, { xPercent: -50, yPercent: -50 });
+    let xTo = gsap.quickTo(cursor.value, "x", { duration: 1, ease: "power3" });
+    let yTo = gsap.quickTo(cursor.value, "y", { duration: 1, ease: "power3" });
+    xTo(pointer.x);
+    yTo(pointer.y);
+    gsap.to(cursor.value, { scale: scale.value });
   });
 </script>
 
