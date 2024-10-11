7b2f2d62718f907e5dc6f9b41ac18cb63f82dd0e
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-10-11 11:37:34 +0400

    Добавил новые модули для сжатия бандла

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   nitro: {
+    compressPublicAssets: true,
+    minify: true,
     esbuild: {
       options: {
         target: 'esnext',
@@ -14,12 +16,41 @@ export default defineNuxtConfig({
     },
   },
 
-  optimization: {
-    //splitChunks: {
-    //  layouts: true,
-    //  pages: true,
-    //  commons: true,
-    //},
+  nuxtPrecompress: {
+    enabled: true, // Enable in production
+    report: false, // set true to turn one console messages during module init
+    test: /\.(js|css|html|txt|xml|svg)$/, // files to compress on build
+    // Serving options
+    middleware: {
+      // You can disable middleware if you serve static files using nginx...
+      enabled: true,
+      // Enable if you have .gz or .br files in /static/ folder
+      enabledStatic: true,
+      // Priority of content-encodings, first matched with request Accept-Encoding will me served
+      encodingsPriority: ['br', 'gzip'],
+    },
+
+    // build time compression settings
+    gzip: {
+      // should compress to gzip?
+      enabled: true,
+      // compression config
+      // https://www.npmjs.com/package/compression-webpack-plugin
+      filename: '[path].gz[query]', // middleware will look for this filename
+      threshold: 10240,
+      minRatio: 0.8,
+      compressionOptions: { level: 9 },
+    },
+    brotli: {
+      // should compress to brotli?
+      enabled: true,
+      // compression config
+      // https://www.npmjs.com/package/compression-webpack-plugin
+      filename: '[path].br[query]', // middleware will look for this filename
+      compressionOptions: { level: 11 },
+      threshold: 10240,
+      minRatio: 0.8,
+    },
   },
   appConfig: {
     buildDate: new Date().toISOString(),
@@ -95,7 +126,14 @@ export default defineNuxtConfig({
   //  injectTo: 'head',
   //  global: true,
   //},
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@vite-pwa/nuxt', ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }], "@nuxt/icon"],
+  modules: [
+    ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }],
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@vite-pwa/nuxt',
+    "@nuxt/icon",
+    "@nuxt/scripts",
+  ],
   imports: { dirs: ['app/stores'] },
   css: ["normalize.css", "~/app/assets/style/global.css"],
   pwa: {
