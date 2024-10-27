06816ef6c46bfca9f79f0d03938f7ac6c8778e17
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-10-27 17:20:21 +0400

    Добавил заготовку под туду, поправил конфик, обновил модуль иконок

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,7 @@ export default defineNuxtConfig({
     },
   ],
   vite: {
-    build:{
+    build: {
       minify: 'terser',
     },
     css: {
---
+++ src/pages/todo/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <!--  TODO LIST -->
+  <div>
+    <nuxt-link class="back" to="/">Назад</nuxt-link>
+    <form>
+      <input type="text" placeholder="Новая задача" />
+      <button>Добавить</button>
+    </form>
+    <ul>
+      <li>
+        <div>
+          <input type="checkbox" />
+          <span>Купить молоко</span>
+          <button>Удалить</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
