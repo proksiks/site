dc7aad91b6bc47be6d06278d567baef8faf1d62a
Khliupnev Vladimir <v.khliupnev@haulmont.com>
2024-11-26 20:08:28 +0400

    Добавил отложенную загрузку заднего фона

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,9 @@
 <template>
   <nuxt-pwa-manifest v-if="isCurrentBranch" />
-  <lazy-cursor-ui />
-  <lazy-canvas-background-ui />
+  <lazy-cursor-ui v-if="showCursor" />
+  <lazy-canvas-background-ui v-if="showBackground" />
+  <lazy-loader-ui v-if="showLoader" />
   <nuxt-layout>
-    <loader-ui />
     <nuxt-page />
   </nuxt-layout>
 </template>
@@ -11,4 +11,13 @@
 <script lang="ts" setup> 
 const runtimeConfig = useRuntimeConfig();
 const isCurrentBranch = computed(() => runtimeConfig.public.CURRENT_BRANCH === "prod");
+const showBackground = ref(false);
+const showCursor = ref(false);
+const showLoader = ref(false);
+
+onMounted(() => {
+  showBackground.value = true;
+  showCursor.value = true;
+  showLoader.value = true;
+})
 </script>
